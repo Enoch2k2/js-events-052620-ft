0acfda6985190a91eed235848dcafa93987e5ecf
Enoch2k2 <enoch2k2@hotmail.com>
2020-09-01 19:40:21 -0500

    added fetch to project

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-
-document.addEventListener("DOMContentLoaded", callOnLoad)
-
 const title = () => document.getElementById("title");
 const form = () => document.querySelector('form');
 const blogTitle = () => document.querySelector('input#blog-title')
 const blogContent = () => document.querySelector('textarea#blog-content')
 const blogList = () => document.getElementById('blog-list')
 
-const blogs = [];
+const baseUrl = 'http://localhost:3000'
+
+document.addEventListener("DOMContentLoaded", callOnLoad)
+
 
 function callOnLoad() {
+  loadBlogs();
   title().addEventListener('click', changeToBlue)
   form().addEventListener('submit', createBlog)
 }
@@ -21,19 +22,63 @@ function changeToBlue() {
   title().style.color = "blue";
 }
 
+function loadBlogs() {
+  // fetch to get our blog data http://localhost:3000/blogs index of blog data
+  // when data comes back we will need to display to page
+
+  console.log('a');
+
+  fetch(baseUrl + '/blogs')
+    .then(resp => {
+      if (resp.status !== 200) {
+        throw new Error(resp.statusText);
+      }
+      console.log('b')
+      return resp.json()
+    })
+    // .then(data => console.log('c', data))
+    // .catch(errors => console.log('d', errors))
+    .then(data => displayBlogs(data))
+
+  console.log('e')
+
+  // 
+}
+
 function createBlog(e) {
   e.preventDefault();
 
-  const blog = {
-    title: blogTitle().value,
-    content: blogContent().value
+  // params.require(:blog).permit(:title, :content)
+
+  const strongParams = {
+    blog: {
+      title: blogTitle().value,
+      content: blogContent().value
+    }
   }
 
-  blogs.push(blog)
+  // "\{'blog': \{ \}\}"
 
-  displayBlog(blog);
+  fetch(baseUrl + '/blogs', {
+    method: "delete",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(strongParams)
+  })
+    .then(resp => resp.json())
+    .then(blog => {
+      displayBlog(blog);
+    })
+
+  // POST /blogs
 
   resetInputs();
+}
+
+function displayBlogs(blogs) {
+  blogs.forEach(blog => displayBlog(blog))
 }
 
 function displayBlog(blog) {
@@ -60,3 +105,12 @@ function resetInputs() {
   blogTitle().value = "";
   blogContent().value = "";
 }
+
+/*
+  Q1: What is the overall process?
+    - Refresh the page, it populates our blogs
+  Q2: What is the cause of this?
+    - When the DOM loads (DOMContentLoaded)
+  Q3: At what point of time should the cause happen?
+    - When the DOM loads (DOMContentLoaded)
+*/
