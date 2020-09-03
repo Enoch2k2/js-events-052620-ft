170aa109fbdd11b4b591aced78b67e2dc0e56b00
Enoch2k2 <enoch2k2@hotmail.com>
2020-09-03 11:17:46 -0500

    added edit and delete

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const form = () => document.querySelector('form');
 const blogTitle = () => document.querySelector('input#blog-title')
 const blogContent = () => document.querySelector('textarea#blog-content')
 const blogList = () => document.getElementById('blog-list')
+const submitButton = () => document.getElementById('submit-blog')
+
 
 const baseUrl = 'http://localhost:3000'
+
+let editing = false;
+let editedBlogId = null;
 
 document.addEventListener("DOMContentLoaded", callOnLoad)
 
@@ -26,21 +31,15 @@ function loadBlogs() {
   // fetch to get our blog data http://localhost:3000/blogs index of blog data
   // when data comes back we will need to display to page
 
-  console.log('a');
-
-  fetch(baseUrl + '/blogs')
+  fetch(baseUrl + '/blogs.json')
     .then(resp => {
       if (resp.status !== 200) {
         throw new Error(resp.statusText);
       }
-      console.log('b')
       return resp.json()
     })
-    // .then(data => console.log('c', data))
-    // .catch(errors => console.log('d', errors))
+    .catch(errors => console.log(errors))
     .then(data => displayBlogs(data))
-
-  console.log('e')
 
   // 
 }
@@ -49,18 +48,50 @@ function createBlog(e) {
   e.preventDefault();
 
   // params.require(:blog).permit(:title, :content)
+  if (editing) {
+    updateBlog();
+  } else {
+    const strongParams = {
+      blog: {
+        title: blogTitle().value,
+        content: blogContent().value
+      }
+    }
+
+    // "\{'blog': \{ \}\}"
+
+    fetch(baseUrl + '/blogs.json', {
+      method: "post",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(strongParams)
+    })
+      .then(resp => resp.json())
+      .then(blog => {
+        displayBlog(blog);
+      })
+
+    // POST /blogs
+
+    resetInputs();
+  }
+}
+
+function updateBlog(e) {
+  let title = document.querySelector('#blog-title').value;
+  let content = blogContent().value;
 
   const strongParams = {
     blog: {
-      title: blogTitle().value,
-      content: blogContent().value
+      title: title,
+      content: content
     }
   }
 
-  // "\{'blog': \{ \}\}"
-
-  fetch(baseUrl + '/blogs', {
-    method: "delete",
+  fetch(baseUrl + '/blogs/' + editedBlogId, {
+    method: "PATCH",
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/json"
@@ -68,13 +99,17 @@ function createBlog(e) {
     body: JSON.stringify(strongParams)
   })
     .then(resp => resp.json())
-    .then(blog => {
-      displayBlog(blog);
+    .then(data => {
+      const div = document.getElementById(editedBlogId).parentNode
+
+      div.querySelector('h4').innerText = data.title
+      div.querySelector('p').innerText = data.content
+
+      resetInputs();
+      editing = false;
+      editedBlogId = null;
+      submitButton().value = "Create Blog";
     })
-
-  // POST /blogs
-
-  resetInputs();
 }
 
 function displayBlogs(blogs) {
@@ -85,12 +120,27 @@ function displayBlog(blog) {
   const div = document.createElement('div');
   const h4 = document.createElement('h4');
   const p = document.createElement('p');
+  const deleteButton = document.createElement('button');
+  const editButton = document.createElement('button');
+
+  deleteButton.classList.add('btn');
+  deleteButton.innerText = 'delete'
+  deleteButton.id = blog.id;
+  deleteButton.addEventListener('click', deleteBlog) // delete /blogs/1
+
+  editButton.classList.add('btn');
+  editButton.innerText = 'edit'
+  editButton.id = blog.id;
+
+  editButton.addEventListener('click', editBlog) // delete /blogs/1
 
   h4.innerText = blog.title;
   p.innerText = blog.content
 
   div.appendChild(h4);
   div.appendChild(p);
+  div.appendChild(editButton)
+  div.appendChild(deleteButton)
 
   blogList().appendChild(div);
   /*
@@ -101,6 +151,34 @@ function displayBlog(blog) {
   */
 }
 
+
+function deleteBlog(e) {
+  this.id // id of blog
+  this.parentNode // div for removing from front end
+
+  fetch(baseUrl + '/blogs/' + this.id, {
+    method: "delete"
+  })
+    .then(resp => {
+      return resp.json();
+    })
+    .then(data => {
+      this.parentNode.remove();
+    })
+}
+
+function editBlog(e) {
+  editing = true;
+
+  // populate form inputs
+  blogTitle().value = this.parentNode.querySelector('h4').innerText
+  blogContent().value = this.parentNode.querySelector('p').innerText;
+  submitButton().value = "Edit Blog"
+
+  editedBlogId = this.id;
+  // debugger;
+}
+
 function resetInputs() {
   blogTitle().value = "";
   blogContent().value = "";
@@ -108,9 +186,9 @@ function resetInputs() {
 
 /*
   Q1: What is the overall process?
-    - Refresh the page, it populates our blogs
+    - click the delete button and it deletes the blog
   Q2: What is the cause of this?
-    - When the DOM loads (DOMContentLoaded)
+    - clicking the delete button
   Q3: At what point of time should the cause happen?
-    - When the DOM loads (DOMContentLoaded)
+    -
 */
